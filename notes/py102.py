5e4df31f64987bab6e5a454673800c473ac6e150
Michael Williams <mikej803@yahoo.com>
2021-04-07 17:07:18 -0400

    update

---
+++ notes/py102.py
@@ -0,0 +1,180 @@
+
+
+# day1 = "Sunday"
+# day2 = "Monday"
+# day3 = "Tuesday"
+# day4 = "Wednesday"
+# day5 = "Thursday"
+# day6 = "Friday"
+# day7 = "Saturday"
+
+# how do we access an element
+# how do we deleste
+# how do we insert
+# how do we update
+# CRUD
+
+
+# myAnimals = ["horse", "dog", "cat", "pot belly pig"]
+
+# print(myAnimals)
+
+# myAnimals[1] = "wolf"
+
+# myAnimals[3] = "cow"
+
+# print(myAnimals)
+
+
+# numAnimals = len(myAnimals) #4
+
+# num = [1, 2, 3]
+
+# print(num)
+# num.append(99)
+
+# print(num)
+
+# print(len(num))
+
+
+
+
+
+
+# print(len(numAnimals))
+
+
+# print(myAnimals[1])
+
+
+
+
+# nums = [4, 4, 0, 9, 6, 5, 2]
+
+# num2 = [2, 3, 1, 7]
+
+# # nums.append(num2)
+
+# nums.extend(num2)
+
+# print(len(nums))
+
+# print(nums[6])
+
+# print(nums)
+
+
+
+
+
+todos = ["walk the dog", "pick the kids up","grocery shopping"]
+
+# print(todos[0])
+# print(todos[1])
+# print(todos[2])
+
+# index = 0
+
+# complete = ''
+# while True:
+#     newTodo = input("enter a new todo item, type 'stop' to complete >>")
+#     if newTodo.lower() == 'stop' :
+#         break
+#     else:
+#         todos.append(newTodo)
+    
+
+# # looping showing contents of our list
+
+# while index < len(todos):
+#     todos[index]
+#     # print(todos[index]) # 0 todos[3]
+#     print(f"{index}: {todos[index]}")
+#     index +=1 # 1
+
+
+# listA = [1, 2, 3, 4]
+
+# listB = [5, 6, 7, 8]
+
+# # listA.extend(listB)
+
+# # print(listA)
+
+# listC = listA +listB
+
+# print(listC)
+
+
+# nums = [5, 6, 7, 7, 8]
+
+# print(len(nums))
+# del nums[2]
+
+# print(nums)
+
+# print(lens(nums))
+
+
+
+# todos = ["pet the cat", "go to work", "shop for groceries", "go home", "feed the cat"]
+
+# print(todos)
+
+# del todos[1:3]
+
+# print(todos)
+
+
+# nums = [1, 2, 3, 4, 5, 6]
+
+# nums.insert(3, 20)
+
+# lastItem = nums.pop()
+
+# nums.index(4)
+
+# print(nums)
+
+# print(lastItem)
+
+# s = int(input('input size of square: '))
+# count = 1
+
+# while count <= s:
+#     print('*' * s)
+#     count += 1
+
+
+
+a = 4
+
+b = a
+
+b = 7
+
+print(a)
+
+print(b)
+
+
+listA = [1, 2, 3, 4]
+
+listB = listA
+
+listB
+# newList = nums [0:3]
+
+# print(newList)
+
+# print(nums)
+
+
+
+
+
+
+
+
+
