9e9d9be9512f8cedf01c392d1e9be81e2743a269
Michael Williams <mikej803@yahoo.com>
2021-04-11 22:51:00 -0400

    update

--- python/homework/lets-speak.py
+++ python/homework/lets-speak.py
@@ -35,25 +35,6 @@ print(output)
 
 
 
-# A => 4
-# E => 3
-# G => 6
-# I => 1
-# O => 0
-# S => 5
-# T => 7
-
-
-# Michael = Michael.replace('A', '4')
-# Michael = Michael.replace('E', '3')
-# Michael = Michael.replace('G', '6')
-# Michael = Michael.replace('I', '1')
-# Michael = Michael.replace('O', '0')
-# Michael = Michael.replace('S', '5')
-# Michael = Michael.replace('T', '7')
-
-
-# print(Michael)
 
 
 
@@ -66,24 +47,4 @@ print(output)
 
 
 
-# Translate a String to leetspeak
-# Example : "I am a leet programmer"
 
-
-
-
-
-
-
-# index = 0
-
-# def translate(phrase):
-#     translation = ""
-#     for letter in phrase:
-#         if letter.upper in "AEGIOST":
-#             translation = translation = "4"
-#         else:
-#             translation = translation + letter
-#     return translation
-
-# print(translate(input("Enter a phrase: ")))
