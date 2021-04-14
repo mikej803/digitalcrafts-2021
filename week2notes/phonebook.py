7631421d9d3b08079472a4475c53ef4d638d6d8d
Michael Williams <mikej803@yahoo.com>
2021-04-14 01:38:31 -0400

    update

--- week2notes/phonebook.py
+++ week2notes/phonebook.py
@@ -1,70 +1,98 @@
 
-
 import pickle
 
-# with open('phonebook')
 
-
-
-    # with open('phonebook.pickle', 'wb') as fh:
-    #     pickle.dump(Electronic_Directory, fh)
-with open('phonebook.pickle', 'rb') as fh:
-    phonebook = pickle.load(fh)
-print(phonebook)
-
-
-# Electronic_Directory = {
+# electronic_phonebook = {
 #     'Igor': '857-485-2935',
 #     'Jazz': '334-584-2345',
 #     'John': '803-793-0866',
 #     'Mark': '757-685-3300',
-#     'Jason': '609-502-1229' 
+#     'Jason': '609-502-1229'
 # }
 
 
 
-
-
-def pickledump():
-with open('phonebook.pickle', 'wb') as fh:
-    pickle.dump(Electronic_Directory, fh)
-
-
-
-
-# userImput = input("Hello, what would you like to do today ")
-
-# ElectricPhonebook = 
-
-# python3 phonebook.py
-while user
-# Electronic Phone Book
-# =====================
-# 1. Look up an entry
-# 2. Set an entry
-# 3. Delete an entry
-# 4. List all entries
-# 5. Quit
-# 1. Look up an entry
-# 2. Set an entry
-# 3. Delete an entry
-# 4. List all entries
-# 5. Quit
-
-# def lookup(name):
-#     print()
-
-# def entry()
-
-# def delete()
-
-# def allEntries()
-
-# def quit()
+with open('phonebook.pickle', 'rb') as fh:
+    electronic_phonebook = pickle.load(fh)
+# with open('phonebook.pickle', 'wb') as fh:
+#     pickle.dump(electronic_phonebook, fh)
+users_input = input('''
+Electonic Phone Book
+===========================
+1. Look up an entry
+2. Set an entry
+3. Delete an entry
+4. List all entries
+5. Quit
+What do you want to do (1-5)?''' )
 
 
 
+#lookingup
+def lookup(name):
+    print(f"\nlook up entry for {name}: {electronic_phonebook[name]}")
+# set a entry
+def set_entry():
+    entry_name = input('\nName? ')
+    entry_number = input('Number? ')
+    electronic_phonebook[entry_name] = entry_number
+    with open('phonebook.pickle', 'wb') as fh:
+        pickle.dump(electronic_phonebook, fh)
+    print(f'Entry stored for: {entry_name}')  #{electronic_phonebook[entry_name]}
+    
+# deleting
+def delete(name):
+    del electronic_phonebook[name]
+    with open('phonebook.pickle', 'wb') as fh:
+        pickle.dump(electronic_phonebook, fh)
+    print(f"You have deleted the entry for {name}")
+# list
+def list():
+    print('Listed entries in phonebook: ')
+    for x in electronic_phonebook:
+        with open('phonebook.pickle', 'rb') as fh:
+            electric_phonebook = pickle.load(fh)
+        print(f'{x}: {electronic_phonebook[x]}')
 
 
 
-# while userInput != 5
+while users_input != '5':
+    users_input = input('Whats next? ')
+
+    if users_input == '1':
+        name = input('What name are you looking for? ')
+        lookup(name)
+    elif users_input == '2':
+        set_entry()
+    elif users_input == '3':
+        name = input('Who would you like to delete? ')
+        delete(name)
+    elif users_input == '4':
+        list()
+    elif users_input == '5':
+        print('Bye')
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
